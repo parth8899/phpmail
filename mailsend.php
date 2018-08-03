6ef652d52091f4f364c2afe95ab773e024135822
parth kamani <parthkamani21@gmail.com>
2018-08-03 16:17:48 +0530

    Add files via upload

---
+++ mailsend.php
@@ -0,0 +1,36 @@
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();                                   // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                    // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                            // Enable SMTP authentication
+$mail->Username = 'parthkamani21@gmail.com';          // SMTP username
+$mail->Password = 'PARTHPBKBSKJBK19943981'; // SMTP password
+$mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                 // TCP port to connect to
+
+$mail->setFrom('parthkamani21@gmail.com', 'parthkamani');
+$mail->addReplyTo('parthkamani21@gmail.com');
+$mail->addAddress('parthkamani21@gmail.com');   // Add a recipient
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+$mail->isHTML(true);  // Set email format to HTML
+
+
+
+
+
+
+$mail->Subject = 'Email from Localhost by CodexWorld';
+$mail->Body    = hello  ;
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
